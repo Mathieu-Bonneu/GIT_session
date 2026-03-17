8e2191432563f81cf5acc26fb9909fd3d0d08168
Mathieu-Bonneu <mathieu.bonneu@ipsa.fr>
2026-03-17 09:02:40 +0100

    Add files via upload

---
+++ Compare.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10;
+    int b = 25;
+
+    if (a > b) {
+        printf("The larger value is: %d\n", a);
+    } else {
+        printf("The larger value is: %d\n", b);
+    }
+
+    return 0;
+}
